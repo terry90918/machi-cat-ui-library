239af8e3270d382deab174329ffd0627cc4bd91a
terry90918 <zxtw17985321@gmail.com>
2023-12-19 16:04:05 +0800

    Update content paths in tailwind.config.js

--- package/tailwind.config.js
+++ package/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./package/src/**/*.{vue,js,ts,jsx,tsx}",
-    "./dev/index.html",
-    "./dev/src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   prefix: 'mc-',
 }
